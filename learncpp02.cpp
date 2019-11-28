8de1ffa5ee6a18d3c9395db519f03d8238b6a5b7
John Paul Villanueva <Jcvillanueva@student.apc.edu.ph>
2019-11-28 02:42:40 +0000

    Uploading Learncpp02.cpp

---
+++ learncpp02.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+	int number;
+	cout << "Enter a Number: ";
+	cin >> number;
+
+	if (number >0)
+	{
+	cout << "You entered a positive Number:" << number << endl;
+	}
+
+	cout << "This state is always executed" << endl;
+	return 0;
+
+}
